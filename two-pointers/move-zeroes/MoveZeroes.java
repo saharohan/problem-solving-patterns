12c9f8ee5bfdf2e03f68fedb0567ad985e4b91d8
Rohan Saha <36517636+saharohan@users.noreply.github.com>
2026-06-09 11:31:15 +0530

    Create MoveZeroes.java

---
+++ two-pointers/move-zeroes/MoveZeroes.java
@@ -0,0 +1,18 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+
+        int write = 0;
+
+        for (int read = 0; read < nums.length; read++) {
+
+            if (nums[read] != 0) {
+
+                int temp = nums[write];
+                nums[write] = nums[read];
+                nums[read] = temp;
+
+                write++;
+            }
+        }
+    }
+}
