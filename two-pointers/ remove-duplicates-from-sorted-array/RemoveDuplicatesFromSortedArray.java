8cc195929c2b3ab5031b3e73a60e4118e7550e3d
Rohan Saha <36517636+saharohan@users.noreply.github.com>
2026-06-12 19:59:49 +0530

    Create RemoveDuplicatesFromSortedArray.java

---
+++ two-pointers/ remove-duplicates-from-sorted-array/RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+
+        int write = 1;
+
+        for (int read = 1; read < nums.length; read++) {
+
+            if (nums[read] != nums[read - 1]) {
+
+                nums[write] = nums[read];
+                write++;
+            }
+        }
+
+        return write;
+    }
+}
