192a16a7c347506181ed3e5adfe631d7b4a83086
Rohan Saha <36517636+saharohan@users.noreply.github.com>
2026-06-17 01:35:25 +0530

    Create PermutationInString.java

---
+++ sliding-window/ permutation-in-string/PermutationInString.java
@@ -0,0 +1,38 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+
+        if (s1.length() > s2.length()) {
+            return false;
+        }
+
+        int[] need = new int[26];
+        int[] window = new int[26];
+
+      
+        for (int i = 0; i < s1.length(); i++) {
+            need[s1.charAt(i) - 'a']++;
+            window[s2.charAt(i) - 'a']++;
+        }
+
+      
+        if (Arrays.equals(need, window)) {
+            return true;
+        }
+
+     
+        for (int right = s1.length(); right < s2.length(); right++) {
+
+           
+            window[s2.charAt(right) - 'a']++;
+
+           
+            window[s2.charAt(right - s1.length()) - 'a']--;
+
+            if (Arrays.equals(need, window)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
